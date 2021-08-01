761af102a1314274ddaddd7bc4fa32a4b031ca61
hmarcuzzo <henrique.souza.m06@gmail.com>
2021-08-01 16:53:33 -0300

    Protocolo de compilação com gRPC

--- Programação com RPC/client/database_pb2.py
+++ Programação com RPC/client/database_pb2.py
@@ -19,31 +19,66 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x0e\x64\x61tabase.proto\"%\n\x05\x43urso\x12\x0e\n\x06\x63odigo\x18\x01 \x01(\x05\x12\x0c\n\x04nome\x18\x02 \x01(\t\"E\n\x05\x41luno\x12\n\n\x02RA\x18\x01 \x01(\x05\x12\x0c\n\x04nome\x18\x02 \x01(\t\x12\x0f\n\x07periodo\x18\x03 \x01(\x05\x12\x11\n\tcod_curso\x18\x04 \x01(\x05\"P\n\nDisciplina\x12\x0e\n\x06\x63odigo\x18\x01 \x01(\t\x12\x0c\n\x04nome\x18\x02 \x01(\t\x12\x11\n\tprofessor\x18\x03 \x01(\t\x12\x11\n\tcod_curso\x18\x04 \x01(\x05\"l\n\tMatricula\x12\n\n\x02RA\x18\x01 \x01(\x05\x12\x16\n\x0e\x63od_disciplina\x18\x02 \x01(\t\x12\x0b\n\x03\x61no\x18\x03 \x01(\x05\x12\x10\n\x08semestre\x18\x04 \x01(\x05\x12\x0c\n\x04nota\x18\x05 \x01(\x02\x12\x0e\n\x06\x66\x61ltas\x18\x06 \x01(\x05\x62\x06proto3'
+  serialized_pb=b'\n\x0e\x64\x61tabase.proto\"z\n\x07Request\x12\x0e\n\x06opCode\x18\x01 \x01(\t\x12\n\n\x02RA\x18\x02 \x01(\x05\x12\x16\n\x0e\x63od_disciplina\x18\x03 \x01(\t\x12\x0b\n\x03\x61no\x18\x04 \x01(\x05\x12\x10\n\x08semestre\x18\x05 \x01(\x05\x12\x0c\n\x04nota\x18\x06 \x01(\x02\x12\x0e\n\x06\x66\x61ltas\x18\x07 \x01(\x05\"P\n\x05\x41luno\x12\n\n\x02RA\x18\x01 \x01(\x05\x12\x0c\n\x04nome\x18\x02 \x01(\t\x12\x0f\n\x07periodo\x18\x03 \x01(\x05\x12\x0c\n\x04nota\x18\x04 \x01(\x02\x12\x0e\n\x06\x66\x61ltas\x18\x05 \x01(\x05\"l\n\tMatricula\x12\n\n\x02RA\x18\x01 \x01(\x05\x12\x16\n\x0e\x63od_disciplina\x18\x02 \x01(\t\x12\x0b\n\x03\x61no\x18\x03 \x01(\x05\x12\x10\n\x08semestre\x18\x04 \x01(\x05\x12\x0c\n\x04nota\x18\x05 \x01(\x02\x12\x0e\n\x06\x66\x61ltas\x18\x06 \x01(\x05\"T\n\x08Response\x12\x10\n\x08response\x18\x01 \x01(\t\x12\x1e\n\nmatriculas\x18\x02 \x03(\x0b\x32\n.Matricula\x12\x16\n\x06\x61lunos\x18\x03 \x03(\x0b\x32\x06.Aluno2:\n\x11RequestMiddleware\x12%\n\x0c\x43omunication\x12\x08.Request\x1a\t.Response\"\x00\x62\x06proto3'
 )
 
 
 
 
-_CURSO = _descriptor.Descriptor(
-  name='Curso',
-  full_name='Curso',
+_REQUEST = _descriptor.Descriptor(
+  name='Request',
+  full_name='Request',
   filename=None,
   file=DESCRIPTOR,
   containing_type=None,
   create_key=_descriptor._internal_create_key,
   fields=[
     _descriptor.FieldDescriptor(
-      name='codigo', full_name='Curso.codigo', index=0,
-      number=1, type=5, cpp_type=1, label=1,
+      name='opCode', full_name='Request.opCode', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='RA', full_name='Request.RA', index=1,
+      number=2, type=5, cpp_type=1, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='nome', full_name='Curso.nome', index=1,
-      number=2, type=9, cpp_type=9, label=1,
+      name='cod_disciplina', full_name='Request.cod_disciplina', index=2,
+      number=3, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='ano', full_name='Request.ano', index=3,
+      number=4, type=5, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='semestre', full_name='Request.semestre', index=4,
+      number=5, type=5, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='nota', full_name='Request.nota', index=5,
+      number=6, type=2, cpp_type=6, label=1,
+      has_default_value=False, default_value=float(0),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='faltas', full_name='Request.faltas', index=6,
+      number=7, type=5, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
@@ -60,7 +95,7 @@ _CURSO = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=18,
-  serialized_end=55,
+  serialized_end=140,
 )
 
 
@@ -94,8 +129,15 @@ _ALUNO = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='cod_curso', full_name='Aluno.cod_curso', index=3,
-      number=4, type=5, cpp_type=1, label=1,
+      name='nota', full_name='Aluno.nota', index=3,
+      number=4, type=2, cpp_type=6, label=1,
+      has_default_value=False, default_value=float(0),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='faltas', full_name='Aluno.faltas', index=4,
+      number=5, type=5, cpp_type=1, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -112,61 +154,8 @@ _ALUNO = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=57,
-  serialized_end=126,
-)
-
-
-_DISCIPLINA = _descriptor.Descriptor(
-  name='Disciplina',
-  full_name='Disciplina',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  create_key=_descriptor._internal_create_key,
-  fields=[
-    _descriptor.FieldDescriptor(
-      name='codigo', full_name='Disciplina.codigo', index=0,
-      number=1, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='nome', full_name='Disciplina.nome', index=1,
-      number=2, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='professor', full_name='Disciplina.professor', index=2,
-      number=3, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='cod_curso', full_name='Disciplina.cod_curso', index=3,
-      number=4, type=5, cpp_type=1, label=1,
-      has_default_value=False, default_value=0,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  serialized_options=None,
-  is_extendable=False,
-  syntax='proto3',
-  extension_ranges=[],
-  oneofs=[
-  ],
-  serialized_start=128,
-  serialized_end=208,
+  serialized_start=142,
+  serialized_end=222,
 )
 
 
@@ -232,22 +221,70 @@ _MATRICULA = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=210,
-  serialized_end=318,
+  serialized_start=224,
+  serialized_end=332,
 )
 
-DESCRIPTOR.message_types_by_name['Curso'] = _CURSO
+
+_RESPONSE = _descriptor.Descriptor(
+  name='Response',
+  full_name='Response',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  create_key=_descriptor._internal_create_key,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='response', full_name='Response.response', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='matriculas', full_name='Response.matriculas', index=1,
+      number=2, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='alunos', full_name='Response.alunos', index=2,
+      number=3, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=334,
+  serialized_end=418,
+)
+
+_RESPONSE.fields_by_name['matriculas'].message_type = _MATRICULA
+_RESPONSE.fields_by_name['alunos'].message_type = _ALUNO
+DESCRIPTOR.message_types_by_name['Request'] = _REQUEST
 DESCRIPTOR.message_types_by_name['Aluno'] = _ALUNO
-DESCRIPTOR.message_types_by_name['Disciplina'] = _DISCIPLINA
 DESCRIPTOR.message_types_by_name['Matricula'] = _MATRICULA
+DESCRIPTOR.message_types_by_name['Response'] = _RESPONSE
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
-Curso = _reflection.GeneratedProtocolMessageType('Curso', (_message.Message,), {
-  'DESCRIPTOR' : _CURSO,
+Request = _reflection.GeneratedProtocolMessageType('Request', (_message.Message,), {
+  'DESCRIPTOR' : _REQUEST,
   '__module__' : 'database_pb2'
-  # @@protoc_insertion_point(class_scope:Curso)
+  # @@protoc_insertion_point(class_scope:Request)
   })
-_sym_db.RegisterMessage(Curso)
+_sym_db.RegisterMessage(Request)
 
 Aluno = _reflection.GeneratedProtocolMessageType('Aluno', (_message.Message,), {
   'DESCRIPTOR' : _ALUNO,
@@ -256,13 +293,6 @@ Aluno = _reflection.GeneratedProtocolMessageType('Aluno', (_message.Message,), {
   })
 _sym_db.RegisterMessage(Aluno)
 
-Disciplina = _reflection.GeneratedProtocolMessageType('Disciplina', (_message.Message,), {
-  'DESCRIPTOR' : _DISCIPLINA,
-  '__module__' : 'database_pb2'
-  # @@protoc_insertion_point(class_scope:Disciplina)
-  })
-_sym_db.RegisterMessage(Disciplina)
-
 Matricula = _reflection.GeneratedProtocolMessageType('Matricula', (_message.Message,), {
   'DESCRIPTOR' : _MATRICULA,
   '__module__' : 'database_pb2'
@@ -270,5 +300,38 @@ Matricula = _reflection.GeneratedProtocolMessageType('Matricula', (_message.Mess
   })
 _sym_db.RegisterMessage(Matricula)
 
+Response = _reflection.GeneratedProtocolMessageType('Response', (_message.Message,), {
+  'DESCRIPTOR' : _RESPONSE,
+  '__module__' : 'database_pb2'
+  # @@protoc_insertion_point(class_scope:Response)
+  })
+_sym_db.RegisterMessage(Response)
+
+
+
+_REQUESTMIDDLEWARE = _descriptor.ServiceDescriptor(
+  name='RequestMiddleware',
+  full_name='RequestMiddleware',
+  file=DESCRIPTOR,
+  index=0,
+  serialized_options=None,
+  create_key=_descriptor._internal_create_key,
+  serialized_start=420,
+  serialized_end=478,
+  methods=[
+  _descriptor.MethodDescriptor(
+    name='Comunication',
+    full_name='RequestMiddleware.Comunication',
+    index=0,
+    containing_service=None,
+    input_type=_REQUEST,
+    output_type=_RESPONSE,
+    serialized_options=None,
+    create_key=_descriptor._internal_create_key,
+  ),
+])
+_sym_db.RegisterServiceDescriptor(_REQUESTMIDDLEWARE)
+
+DESCRIPTOR.services_by_name['RequestMiddleware'] = _REQUESTMIDDLEWARE
 
 # @@protoc_insertion_point(module_scope)
