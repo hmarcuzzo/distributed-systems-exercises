033d5a3cd16f0a0b988804911240f46fb3e97372
sorattorafa <soratto@alunos.utfpr.edu.br>
2021-08-13 20:53:44 -0300

    create responses types

--- Programação com RMI/src/main/java/Response.java
+++ Programação com RMI/src/main/java/Response.java
@@ -16,6 +16,9 @@ public class Response implements Serializable {
   private int response_status_code;
   private String error;
   private String response_data;
+  private NotasByAluno[] notasByAlunoResponse;
+  private Aluno[] alunosByDisciplina;
+  private NotasByDisciplina[] notasByDisciplina;
 
   public Response(){
     System.out.println("constructor with no parameter");
@@ -30,6 +33,18 @@ public class Response implements Serializable {
     return this.response_status_code;
   }
 
+  public NotasByAluno[] get_notas_by_aluno(){
+    return this.notasByAlunoResponse;
+  }
+
+  public Aluno[] get_alunos_by_disciplina(){
+    return this.alunosByDisciplina;
+  }
+
+  public NotasByDisciplina[] get_notas_by_disciplina(){
+    return this.notasByDisciplina
+  }
+
   public String get_error() {
     return this.error;
   }
@@ -41,4 +56,37 @@ public class Response implements Serializable {
   public Response get(){
     return this;
   }
+}
+
+
+public class NotasByAluno {
+    private String cod_disciplina;
+    private int ano;
+    private int semestre;
+    private float nota;
+    private int faltas;
+
+  public NotasByAluno(){
+    System.out.println("constructor with no parameter");
+  }
+
+}
+
+public class Aluno {
+    private int RA;
+    private String nome;
+    public Aluno(){
+      System.out.println("constructor with no parameter");
+    }
+}
+
+
+
+public class NotasByDisciplina {
+    private int RA;
+    private float nota;
+
+    public NotasByDisciplina(){
+      System.out.println("constructor with no parameter");
+    }
 }
