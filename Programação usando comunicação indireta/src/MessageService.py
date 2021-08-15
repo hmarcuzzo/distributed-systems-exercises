bf01897ed9628a8577ac61b1a5da6c40a0cb4420
hmarcuzzo <henrique.souza.m06@gmail.com>
2021-08-15 18:19:38 -0300

    template

---
+++ Programação usando comunicação indireta/src/MessageService.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+    
+class MessageService():
+    def __init__(self, filename):
+        self.data = pd.read_csv(filename)
+        self.classify_queue = []
+        self.buffer = []
+
+    def consume_queue(queue, item):
+        pass
+
+    
+    def insert_in_queue():
+        
+
+    def classify():
+        pass
